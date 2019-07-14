0199011dffc3b4d61a15b2e3e561c7b9aa3e9366
Feather Knee <fknee@nvidia.com>
2019-07-14 11:22:40 -0700

    simple example of typeahead

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
+const baseApiUrl = 'https://jsonplaceholder.typicode.com/todos/'
 
 function App() {
+  // useEffectOnce(() => {
+  //   setData(JSON.parse(localStorage.getItem('data')))
+  // })
+  // allows use to get and set the value of param
+  const [param, setParam] = useState('')
+  // allows use to get and set the value of data
+  const [data, setData] = useState({})
+
+  const fetchData = param => {
+    const url = `${baseApiUrl}${param}`
+    return fetch(url, {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => setData(data))
+  }
+
+  // response to a change in the value of param
+  useEffect(
+    () => {
+      console.log('hihi', param)
+      // if param length is greater than 1 character
+      if (param.length) fetchData(param)
+    },
+    [param]
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hooks + RxJs</h2>
+      <input defaultValue={param} onChange={e => setParam(e.target.value)} />
+      {data &&
+        <div>
+          <input type="checkbox" checked={data.completed} />
+          {data.title}
+        </div>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

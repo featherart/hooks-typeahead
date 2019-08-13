d1df4d3b8f13636c8a7d0b6a3d265047767706f6
Feather Knee <fknee@nvidia.com>
2019-08-13 15:30:31 -0700

    update loading and readme instructions

--- src/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Loading = () =>
-<div>
-  <h1>Loading ...</h1>
-  <img src='/swings.gif' />
-</div>
---
+++ src/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loading.css'
+
+export const Loading = () => (
+	<div class='loading-container'>
+		<span class='text'>Loading...</span>
+		<span class='loading'>
+			<span class='center' />
+		</span>
+	</div>
+)
